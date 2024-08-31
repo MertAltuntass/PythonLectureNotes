dae5dacf0ef65b28f1df9844857ae93f9e9cf313
Sabre <95714525+MertAltuntass@users.noreply.github.com>
2024-08-31 17:01:26 +0300

    01-Variables
    01-Variables

---
+++ 01-Variables(Değişkenler).py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+4+5
+
+
+# In[7]:
+
+
+1500+500
+
+
+# In[9]:
+
+
+800/7
+
+
+# In[11]:
+
+
+500-260
+
+
+# In[13]:
+
+
+4912*4
+
+
+# In[15]:
+
+
+2^2
+
+
+# In[17]:
+
+
+2*2*2
+
+
+# In[19]:
+
+
+2**3
+
+
+# In[21]:
+
+
+10 % 2 
+
+
+# In[23]:
+
+
+11 % 2 
+
+
+# Integer
+
+# In[26]:
+
+
+3+3
+
+
+# Float
+
+# In[29]:
+
+
+0.4 * 3.2
+
+
+# İşlem sonunda 2 yazmasının sebebi tamamen python işleyişiyle alakalı bir durum.
+
+# In[33]:
+
+
+2+2
+
+
+# Variables (Değişkenler)
+
+# In[36]:
+
+
+x = 3 
+
+
+# In[38]:
+
+
+y = 5 
+
+
+# In[40]:
+
+
+x+y
+
+
+# In[42]:
+
+
+x**y
+
+
+# In[44]:
+
+
+x=4
+
+
+# In[46]:
+
+
+x+y
+
+
+# In[48]:
+
+
+x**y
+
+
+# In[50]:
+
+
+r = input("r: ")
+
+
+# In[52]:
+
+
+r*2*3.14
+
+
+# In[54]:
+
+
+type(x)
+
+
+# In[56]:
+
+
+pi = 3.14
+
+
+# In[58]:
+
+
+type(pi)
+
+
+# In[60]:
+
+
+type(r)
+
+
+# In[62]:
+
+
+r
+
+
+# In[64]:
+
+
+r_int = int(r)
+
+
+# In[66]:
+
+
+r_int
+
+
+# In[68]:
+
+
+type(r_int)
+
+
+# In[70]:
+
+
+r_int*pi*2
+
+
+# In[ ]:
+
+
+
+
