a4e96130a66ee423dc80252e7685c70941025038
Sabre <95714525+MertAltuntass@users.noreply.github.com>
2024-09-02 21:16:29 +0300

    02.09.2024

---
+++ 02-String/02-Strings.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+"Metin2 pişmanlıktır"
+
+
+# In[3]:
+
+
+'Wow candır'
+
+
+# In[5]:
+
+
+'i'm a pilot
+
+
+# In[7]:
+
+
+"i'm a pilot "
+
+
+# In[9]:
+
+
+x = "Mert"
+
+
+# In[11]:
+
+
+x
+
+
+# In[13]:
+
+
+x = "Hey"
+
+
+# In[15]:
+
+
+x
+
+
+# In[17]:
+
+
+x = 3 
+
+
+# In[19]:
+
+
+x
+
+
+# In[21]:
+
+
+x = "Mert"
+
+
+# In[23]:
+
+
+type(x)
+
+
+# In[25]:
+
+
+len(x)
+
+
+# length
+
+# In[28]:
+
+
+len(x)
+
+
+# In[31]:
+
+
+print(x)
+
+
+# Escape Characters
+
+# In[34]:
+
+
+print("xxxxxxxxx")
+
+
+# In[36]:
+
+
+print("Mert
+altuntaş")
+
+
+# In[38]:
+
+
+print("Mert \nAltuntas")
+
+
+# In[40]:
+
+
+print("Mert \tAltuntas")
+
+
+# In[ ]:
+
+
+
+
---
+++ 03-StringsAdvanced/03-StringsAdvanced.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_string = "Mert ALTUNTAS"
+
+
+# In[3]:
+
+
+my_string
+
+
+# ##indexing
+
+# In[6]:
+
+
+my_string[0]
+
+
+# In[8]:
+
+
+my_string[3]
+
+
+# In[10]:
+
+
+my_string[-1]
+
+
+# In[12]:
+
+
+my_string[-2]
+
+
+# In[14]:
+
+
+my_string_2 = "1234567890"
+
+
+# In[16]:
+
+
+my_string_2[0]
+
+
+# In[18]:
+
+
+my_string_2[2:]
+
+
+# #Slicing
+
+# In[23]:
+
+
+my_string_2[4:]
+
+
+# In[25]:
+
+
+my_string_2[:2]
+
+
+# ##stopping index
+
+# In[28]:
+
+
+my_string_2[:4]
+
+
+# In[30]:
+
+
+my_string_2[2:4]
+
+
+# In[32]:
+
+
+my_string_2[5:8]
+
+
+# ##step size
+
+# In[35]:
+
+
+my_string_2[::]
+
+
+# In[37]:
+
+
+my_string_2[::3]
+
+
+# In[41]:
+
+
+my_string_2[::2]
+
+
+# In[43]:
+
+
+my_string_2[2:4:2] ##2 başla 4 dur 2şer atla
+
+
+# In[47]:
+
+
+my_string_2[::-1] ##tersine çevirmiş oluyoruz
+
+
+# In[ ]:
+
+
+
+
