a4662b05988a6f459ca53c990c856f7be06eb5e2
Sabre <95714525+MertAltuntass@users.noreply.github.com>
2024-09-07 16:39:43 +0300

    07.09.2024 Dictionary - Set

---
+++ 05-Dictionary/05-Dictionary.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+my_dictionary = {"key":"value"}
+
+
+# In[4]:
+
+
+my_dictionary["key"]
+
+
+# In[6]:
+
+
+my_fitness_dictionary = {"run":100,"swim":200}
+
+
+# In[8]:
+
+
+my_fitness_dictionary["run"]
+
+
+# In[10]:
+
+
+my_fitness_dictionary["swim"]
+
+
+# In[12]:
+
+
+my_dictionary_2 = {"key1":1,"key2":2,"key3":"apple"}
+
+
+# In[14]:
+
+
+my_dictionary_2["key1"]
+
+
+# In[16]:
+
+
+my_dictionary_2["key3"]
+
+
+# In[18]:
+
+
+my_dictionary_3 = {"key1":10,20:30}
+
+
+# In[20]:
+
+
+my_dictionary_3["key1"]
+
+
+# In[22]:
+
+
+my_dictionary_3[20]
+
+
+# In[26]:
+
+
+my_dictionary_4 = {"key1":100, "key2": [10,20,30], "key3": {"a":5}}
+
+
+# In[28]:
+
+
+my_dictionary_4.keys()
+
+
+# In[30]:
+
+
+my_dictionary_4.values()
+
+
+# In[40]:
+
+
+my_dictionary_4["key3"]["a"]
+
+
+# In[42]:
+
+
+my_dic={"k1":1,"k2":2}
+
+
+# In[44]:
+
+
+my_dic["k1"]=3
+
+
+# In[46]:
+
+
+my_dic
+
+
+# In[48]:
+
+
+my_dic["k3"]=7
+
+
+# In[50]:
+
+
+my_dic
+
+
+# In[ ]:
+
+
+
+
---
+++ 06-Set/06-Set.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##Aynı elemandan birden fazla olamıyor
+
+# In[2]:
+
+
+my_list=[1,2,3,1]
+
+
+# In[4]:
+
+
+my_list
+
+
+#  ##casting
+#  
+
+# In[7]:
+
+
+my_set=set(my_list)
+
+
+# In[9]:
+
+
+my_set
+
+
+# In[11]:
+
+
+my_set2={1,2,3,1}
+
+
+# In[13]:
+
+
+my_set2
+
+
+# In[15]:
+
+
+type(my_set2)
+
+
+# In[17]:
+
+
+my_set3={"a","b","a"}
+
+
+# In[19]:
+
+
+my_set3
+
+
+# In[21]:
+
+
+my_list=[]
+
+
+# In[23]:
+
+
+type(list)
+
+
+# In[25]:
+
+
+my_list.append(1)
+
+
+# In[27]:
+
+
+my_list
+
+
+# In[29]:
+
+
+my_set_4={}
+
+
+# In[31]:
+
+
+my_set_4
+
+
+# In[33]:
+
+
+type(my_set_4)
+
+
+# In[35]:
+
+
+my_set_5=set()
+
+
+# In[37]:
+
+
+type(my_set_5)
+
+
+# In[39]:
+
+
+my_set_5.add(1)
+
+
+# In[41]:
+
+
+my_set_5.add(2)
+
+
+# In[43]:
+
+
+my_set_5
+
+
+# In[45]:
+
+
+my_set_5.add(2)
+
+
+# In[47]:
+
+
+my_set_5
+
+
+# In[49]:
+
+
+my_dict_x1=dict()
+
+
+# In[51]:
+
+
+my_dict_x1["key1"]=1
+
+
+# In[53]:
+
+
+my_dict_x1
+
+
+# In[ ]:
+
+
+
+
