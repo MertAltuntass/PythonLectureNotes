3d7849094dcd9079a5d21caa7ae14917878308a2
Sabre <95714525+MertAltuntass@users.noreply.github.com>
2024-09-12 18:00:29 +0300

    Comparison

---
+++ 11-Comparison/11-Comparison.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x =5 
+
+
+# In[3]:
+
+
+y = 4 
+
+
+# In[5]:
+
+
+x > y
+
+
+# In[7]:
+
+
+x < y
+
+
+# In[9]:
+
+
+x >= y
+
+
+# In[11]:
+
+
+x <= y
+
+
+# In[13]:
+
+
+x >= 5 
+
+
+# In[15]:
+
+
+x >= 5.0
+
+
+# In[17]:
+
+
+x >= 5.1
+
+
+# In[19]:
+
+
+x >= 4.1
+
+
+# In[21]:
+
+
+x == y
+
+
+# In[23]:
+
+
+x != y ## ! varsa olumsuz anlamı vardır eşit değilse anlamına gelir genelde boolean için kullanılır.
+
+
+# In[25]:
+
+
+## and or not 
+
+
+# In[33]:
+
+
+2 > 1  and 3 > 2 ## and konusunda 2 koşulunda doğru olması gerekiyor.
+
+
+# In[31]:
+
+
+2 < 1 and 3 > 2 
+
+
+# In[35]:
+
+
+2 < 1 or 3 > 2 ##or konusunda 1 koşulun doğru olması yeterli.
+
+
+# In[37]:
+
+
+not  10== 10 ## tamamen olumsuz anlam katıyor true alan false oluyor false olan true oluyor.
+
+
+# In[ ]:
+
+
+
+
